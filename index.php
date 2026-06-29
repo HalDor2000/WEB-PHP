43763caca95ef00718fa46a8681a838b20889138
Jorge Aguilar H. <139644060+HalDor2000@users.noreply.github.com>
2026-06-29 15:17:09 -0600

    Estructura Web

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/post.php">Posts</a></li>
+        <li><a href="/about.php">Acerca de</a></li>
+        <li><a href="/links">Proyectos</a></li>
+    </ul>
+    <h1>Bienvenido a la pagina de Inicio</h1>
+    <p> Esta es la pagina principal de nuestro sitio web</p>
+</body>
+</html>
